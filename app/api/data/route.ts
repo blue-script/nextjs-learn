af1aa2fd48e8ad23e07ceb600f26c592404e6871
blue-script <alexm92@inbox.ru>
2024-06-15 19:35:30 +0500

    feat: : any

---
+++ app/api/data/route.ts
@@ -0,0 +1,17 @@
+export async function GET(request) {
+  const data = {
+    name: 'Bob',
+    age: 25,
+    bio: 'Some info'
+  }
+
+  return new Response(JSON.stringify(data));
+}
+
+export async function POST(request: any) {
+  return new Response(request.body);
+}
+
+export async function DELETE(request: any) {
+  return new Response(request.body);
+}
